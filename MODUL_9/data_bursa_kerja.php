f5c9f81fa37bace8e976b8d3020d07c832250fc2
jaeminabfme <20230910085@uniku.ac.id>
2025-01-26 08:27:03 +0700

    Add files via upload

---
+++ MODUL_9/data_bursa_kerja.php
@@ -0,0 +1,22 @@
+<?php
+return [
+    [
+        'perusahaan' => 'PT. ABC',
+        'posisi' => 'Software Developer',
+        'lokasi' => 'Jakarta',
+        'kontak' => 'hrd@abc.com',
+    ],
+    [
+        'perusahaan' => 'PT. XYZ',
+        'posisi' => 'Graphic Designer',
+        'lokasi' => 'Bandung',
+        'kontak' => 'recruitment@xyz.com',
+    ],
+    [
+        'perusahaan' => 'PT. FANI',
+        'posisi' => 'Software Developer',
+        'lokasi' => 'KUNINGAN',
+        'kontak' => 'hrd@abc.com',
+    ],
+];
+?>
